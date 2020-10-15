2748c01335a06138503e44543a9a64dd192beb7b
Gaurish <gaurish108@gmail.com>
2020-10-15 09:27:39 -0400

    Added the list of code-chunk names

--- src/tspnng.py
+++ src/tspnng.py
@@ -1,2 +1,53 @@
-#line 2 "mainlitprog.nw"
-print("Hello World")
+
+#line 23 "mainlitprog.nw"
+from matplotlib import matplotlib.pyplot as plt
+import matplotlib as mpl
+from matplotlib import rc
+rc('font',**{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+
+import scipy as sp
+import numpy as np
+import random
+from colorama import Fore, Back, Style
+#line 42 "mainlitprog.nw"
+   
+#line 51 "mainlitprog.nw"
+def uniform_points(numpts):
+     return  sp.rand(numpts, 2).tolist()
+
+def non_uniform_points(numpts):
+
+    cluster_size = int(np.sqrt(numpts)) 
+    numcenters   = cluster_size
+    centers      = sp.rand(numcenters,2).tolist()
+    scale, points = 4.0, []
+
+    for c in centers:
+        cx, cy = c[0], c[1]
+        sq_size      = min(cx,1-cx,cy, 1-cy)
+
+        loc_pts_x    = np.random.uniform(low  = cx-sq_size/scale, 
+                                         high = cx+sq_size/scale, 
+                                         size = (cluster_size,))
+        loc_pts_y    = np.random.uniform(low = cy-sq_size/scale, 
+                                         high = cy+sq_size/scale, 
+                                         size = (cluster_size,))
+
+        points.extend(zip(loc_pts_x, loc_pts_y))
+
+    num_remaining_pts = numpts - cluster_size * numcenters
+    remaining_pts = scipy.rand(num_remaining_pts, 2).tolist()
+    points.extend(remaining_pts)
+    return points
+#line 43 "mainlitprog.nw"
+   
+#line 95 "mainlitprog.nw"
+def write_to_yaml_file(data, dir_name, file_name):
+   import yaml
+   with open(dir_name + '/' + file_name, 'w') as outfile:
+          yaml.dump( data, outfile, default_flow_style = False)
+
+
+
+
