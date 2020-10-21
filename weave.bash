34f670119d88cc89973952f7c33105ee7aed47d7
Gaurish <gaurish108@gmail.com>
2020-10-21 14:59:46 -0400

    Added a separate folder for images generated with Asymptote, TikZ, Povray etc.

--- weave.bash
+++ weave.bash
@@ -5,6 +5,7 @@ noweave  -n -index -latex mainlitprog.nw > mainlitprog.tex
 git add mainlitprog.tex
 pdflatex -shell-escape -interaction=nonstopmode main.tex
 bibtex main.aux
+asy *.asy
 pdflatex -shell-escape -interaction=nonstopmode main.tex
 pdflatex -shell-escape -interaction=nonstopmode main.tex
 ./clean.sh
